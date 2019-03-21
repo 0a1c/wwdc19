fc6e81b0cd023acc5c8e216afcc50f0324145d10
anandchandra50 <anandchandra50@gmail.com>
2019-03-20 23:48:36 -0700

    working on ipad

--- wwdc2019.playground/Contents.swift
+++ wwdc2019.playground/Contents.swift
@@ -5,23 +5,28 @@ import PlaygroundSupport
 
 
 class VisionViewController: UIViewController {
+    // ui elements
+    private var imageView: UIImageView = UIImageView()
+    private var prescriptionSlider: UISlider = UISlider()
+    private var currentPrescriptionLabel: UILabel = UILabel()
+    private var arButton: UIButton = UIButton()
+    private var nextImageButton: UIButton = UIButton()
+    private var controlBackground: UIView = UIView()
     
-    private var imageView: UIImageView!
-    private var prescriptionSlider: UISlider!
+    /// currently simulating prescription
     private var prescription: Double = 0
-    private var currentPrescriptionLabel: UILabel!
-    private var arButton: UIButton!
-    private var nextImageButton: UIButton!
     
+    /// all images to display
     private var displayImages: [UIImage] = []
+    /// currently displayed image
     private var currentImageIndex: Int = 0
 
+    // width and height of screen
     private var width: CGFloat!
     private var height: CGFloat!
-    var baseView: UIView!
 
     override func loadView() {
-        displayImages = [UIImage(named: "applepark.jpg")!, UIImage(named: "wwdc.jpg")!, UIImage(named: "poster.jpg")!]
+        displayImages = [UIImage(named: "applepark.png")!, UIImage(named: "wwdc.png")!, UIImage(named: "poster.png")!]
         let view = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
         view.backgroundColor = UIColor.white
         self.view = view
@@ -29,23 +34,40 @@ class VisionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initAll()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        updateAll()
+    }
+    
+    /// initialize all ui elements
+    private func initAll() {
         width = self.view.frame.width
         height = self.view.frame.height
-        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: width, height: height))
-        imageView.frame
-        view.frame
         view.addSubview(imageView)
-
-        displayImage(UIImage(named: "wwdc.jpg")!, prescription: 0)
+        displayImage(displayImages[currentImageIndex], prescription: 0)
         
         initControlBackground()
         initSlider()
         initLabel()
         initButtons()
+    }
+    
+    /// update frames for all ui elements
+    private func updateAll() {
+        width = self.view.frame.width
+        height = self.view.frame.height
+        updateImageView()
+        updateControlBackground()
+        updateSlider()
+        updateLabel()
+        updateButtons()
+    }
+    
+    private func updateImageView() {
+        imageView.frame = CGRect(x: 0, y: 0, width: width, height: height)
     }
     
     /// Displays the given image
@@ -57,41 +79,41 @@ class VisionViewController: UIViewController {
         guard let resizedImage = largeImage.withSize(targetSize: CGSize(width: 2 * width, height: 2 * heightRatio * width)) else { return }
         let prescriptionImage = resizedImage.withPrescription(perscription: prescription, original: resizedImage.size)
         imageView.image = prescriptionImage
-        imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight, .flexibleBottomMargin, .flexibleRightMargin, .flexibleLeftMargin, .flexibleTopMargin]
+        imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight, .flexibleBottomMargin, .flexibleRightMargin, .flexibleLeftMargin]
         imageView.layer.masksToBounds = true
         imageView.contentMode = .scaleAspectFill
     }
     
     private func initControlBackground() {
-        height
-        let controlBackground = UIView(frame: CGRect(x: 0, y: height * 0.9, width: width, height: height * 0.2))
         controlBackground.layer.cornerRadius = 20
         controlBackground.layer.masksToBounds = true
         controlBackground.backgroundColor = darkGrayColor
         view.addSubview(controlBackground)
     }
     
+    private func updateControlBackground() {
+        controlBackground.frame = CGRect(x: 0, y: height * 0.9, width: width, height: height * 0.2)
+        
+    }
+    
     /// initialize the slider
     private func initSlider() {
-        imageView.bounds.maxY
-        imageView.bounds.maxY * 0.2
-        height
-        let widthScale: CGFloat = 0.7
-        prescriptionSlider = UISlider(frame: CGRect(x: CGFloat((1 - widthScale) / 2) * width, y: height * 0.95, width: width * widthScale, height: height * 0.04))
-        prescriptionSlider.frame
         prescriptionSlider.maximumValue = 4
         prescriptionSlider.minimumValue = 0
         prescriptionSlider.tintColor = offwhiteColor
         prescriptionSlider.value = 0
         prescriptionSlider.maximumTrackTintColor = UIColor.darkGray
-        prescriptionSlider.transform = CGAffineTransform(scaleX: 0.666, y: 0.666)
         prescriptionSlider.addTarget(self, action: #selector(updatePrescription(sender:)), for: .valueChanged)
         view.addSubview(prescriptionSlider)
     }
+    
+    private func updateSlider() {
+        let widthScale: CGFloat = 0.4
+        prescriptionSlider.frame = CGRect(x: CGFloat(0.5 - widthScale / 2) * width, y: height * 0.95, width: width * widthScale, height: height * 0.04)
+        prescriptionSlider.transform = CGAffineTransform(scaleX: 0.666, y: 0.666)
+    }
 
     private func initLabel() {
-        let widthScale: CGFloat = 0.6
-        currentPrescriptionLabel = UILabel(frame: CGRect(x: CGFloat((1 - widthScale) / 2) * width, y: height * 0.91, width: width * widthScale, height: height * 0.03))
         currentPrescriptionLabel.textAlignment = .center
         currentPrescriptionLabel.font = UIFont(name: "Avenir-Medium", size: 18)
         currentPrescriptionLabel.textColor = offwhiteColor
@@ -99,20 +121,27 @@ class VisionViewController: UIViewController {
         view.addSubview(currentPrescriptionLabel)
     }
     
+    private func updateLabel() {
+        let widthScale: CGFloat = 0.6
+        currentPrescriptionLabel.frame = CGRect(x: CGFloat((1 - widthScale) / 2) * width, y: height * 0.91, width: width * widthScale, height: height * 0.03)
+    }
+    
     private func initButtons() {
-        let widthScale: CGFloat = 0.15
-        nextImageButton = UIButton(frame: CGRect(x: (0.125 - widthScale / 2) * width, y: 0.925 * height, width: width * widthScale, height: height * 0.05))
         nextImageButton.setImage(UIImage(named: "picture")?.tint(with: offwhiteColor), for: .normal)
         nextImageButton.imageView?.contentMode = .scaleAspectFit
         nextImageButton.addTarget(self, action: #selector(nextImage), for: .touchUpInside)
         view.addSubview(nextImageButton)
         
-        arButton = UIButton(frame: CGRect(x: (0.875 - widthScale / 2) * width, y: 0.925 * height, width: width * widthScale, height: height * 0.05))
         arButton.setImage(UIImage(named: "camera")?.tint(with: offwhiteColor), for: .normal)
         arButton.imageView?.contentMode = .scaleAspectFit
         arButton.addTarget(self, action: #selector(switchToAR), for: .touchUpInside)
         view.addSubview(arButton)
-        
+    }
+    
+    private func updateButtons() {
+        let widthScale: CGFloat = 0.15
+        nextImageButton.frame = CGRect(x: (0.125 - widthScale / 2) * width, y: 0.925 * height, width: width * widthScale, height: height * 0.05)
+        arButton.frame = CGRect(x: (0.875 - widthScale / 2) * width, y: 0.925 * height, width: width * widthScale, height: height * 0.05)
     }
     
     @objc func switchToAR() {
@@ -136,6 +165,10 @@ class VisionViewController: UIViewController {
     }
 }
 
+/// Created attributed string for prescription label (highlight value)
+///
+/// - Parameter prescription: prescription
+/// - Returns: the attributed string
 func stringForPrescription(prescription: Double) -> NSAttributedString {
     let prescriptionValue = String(format: "%.1f", prescription)
     let baseAttrs: [NSAttributedString.Key: Any] = [ NSAttributedString.Key.foregroundColor: offwhiteColor, NSAttributedString.Key.font: UIFont(name: "Avenir-Medium", size: 16)! ]
@@ -147,10 +180,6 @@ func stringForPrescription(prescription: Double) -> NSAttributedString {
 }
 
 
-
-//PlaygroundPage.current.needsIndefiniteExecution = true
-
-// Present the view controller in the Live View window
 let visionVC = VisionViewController()
 PlaygroundPage.current.liveView = visionVC
 
